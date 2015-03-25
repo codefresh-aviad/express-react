bf42636c34ef6a387e114caa4cf487e922afb07e
codefresh-aviad <aviad@codefresh.io>
2015-03-25 14:18:29 +0000

    fix

--- api/comments.js
+++ api/comments.js
@@ -1,16 +1,25 @@
 var express	= require('express'),
-	router  = express.Router();
+	router  = express.Router(),
+	comments = require('../public/comments.json');
 	
 	
-var comments = require('../public/comments.json');	
+var comments_helper = {
+	comments: comments,
+	get: function() {
+		return comments_helper.comments;
+	},
+	set: function(data) {
+		comments.unshift(data);
+	}
+};	
 	
 router.get('/', function(req,res){
-	res.send(comments);
+	res.send(comments_helper.get());
 });	
 
 router.post('/', function(req,res){
-	console.log(req.body);
-	res.send(comments);
+	comments_helper.set(req.body);
+	res.send(comments_helper.get());
 });	
 	
 	
--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var express = require('express'),
-	app 	= express(),
-	server 	= require('http').createServer(app),
-	bodyParser = require('body-parser'),
-	path 	= require('path');
+var express 	= require('express'),
+	app 		= express(),
+	server 		= require('http').createServer(app),
+	bodyParser 	= require('body-parser'), //support POST requests
+	multer 		= require('multer'), //support POST requests
+	path 		= require('path');
+	
 // set default server port
 var port = process.env.PORT || 9000;
 
 app.set('view engine', 'html');
 
-app.use(express.urlencoded());
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
 
 app.use('/api/comments', require('./api/comments'));
 
@@ -18,7 +22,6 @@ app.get('/', function(req, res){
 
 app.use(express.static(__dirname + '/public'));
 
-// start the server
 app.listen(port, function(){
 	console.log('Express server listening on %d', port);
 });
