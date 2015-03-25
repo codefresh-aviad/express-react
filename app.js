e91291ec64f31e8b16d2e17f46e5a001e36a85a6
codefresh-aviad <aviad@codefresh.io>
2015-03-25 11:54:37 +0000

    progress

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ var port = process.env.PORT || 9000;
 app.set('view engine', 'html');
 // set client index url (view)
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/app/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
+app.use(express.static(__dirname + '/public'));
+
 // start the server
-server.listen(port, function(){
-    console.log('Express server listening on %d', port);
+app.listen(port, function(){
+	console.log('Express server listening on %d', port);
 });
